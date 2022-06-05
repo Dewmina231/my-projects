8c95a1b2147c8f4fecaaf7afa3da1418738e5e4c
Dewmina231 <98499224+Dewmina231@users.noreply.github.com>
2022-06-05 16:47:13 +1000

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,21 +1,22 @@
 
-#This program asks the user if they want to take a video or a photo and i does so accordingly
+#This program asks the user if they want to take a video or a photo and it does so accordingly
 #I built this on a rasberry pi 4 
 
-
+#imports libraries
 from picamera import PiCamera
 import time as t
 
 
 
 
-
+#setting up the camera
 camera = PiCamera()
 camera.resolution = (640, 480)
 
-
+#asks the user if they want a video or photo
 g = input("Video or photo??[v/p]: ")
 
+#if the answer is = v it will start taking a video and put it into a folder
 if g == "v":
     r = "recording start"
     camera.start_preview()
@@ -26,6 +27,7 @@ if g == "v":
     print("recording stopped")
     camera.stop_recording()
     exit()
+#if the user wants a picture it will take one and save it.
 else:
     camera.start_preview()
     print("your photo will be taken soon")
