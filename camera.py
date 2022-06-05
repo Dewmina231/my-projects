9be9b2720746b9d6f63d251bb948dbf7b86f012f
Dewmina231 <98499224+Dewmina231@users.noreply.github.com>
2022-06-05 16:43:13 +1000

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,31 @@
+from picamera import PiCamera
+import time as t
+
+
+
+
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+
+
+g = input("Video or photo??[v/p]: ")
+
+if g == "v":
+    r = "recording start"
+    camera.start_preview()
+    t.sleep(2)
+    print("recording start")
+    camera.start_recording("my_movie.h264")
+    t.sleep(8)
+    print("recording stopped")
+    camera.stop_recording()
+    exit()
+else:
+    camera.start_preview()
+    print("your photo will be taken soon")
+    t.sleep(4)
+    camera.capture("your_photo.jpg")
+    print("done")
+    t.sleep(2)
+    exit()
